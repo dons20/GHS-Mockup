b56f3e35205c57ef18ff70ddc17fa3283e652b37
Keno Clayton <keno.clayton15@gmail.com>
2017-02-10 17:32:22 -0500

    Modified behaviour of mobile menu and toggle

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+    var $offCanvas = $("#mobile-menu");
     var $hamburger = $(".hamburger");
+
+    $offCanvas.on("hide", function(e) {
+        $hamburger.toggleClass("is-active");
+    })
     $hamburger.on("click", function(e) {
         $hamburger.toggleClass("is-active");
-        // Do something else, like open/close menu
     });
 });
