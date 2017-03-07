6ead44b693ab800ac77ad8fabb6598343b5a55d3
Keno Clayton <keno.clayton15@gmail.com>
2017-03-07 16:14:09 -0500

    Modified navigation behaviour

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
     var $offCanvas = $("#mobile-menu");
     var $hamburger = $(".hamburger");
+    var current = location.pathname;
+    console.log(current);
+
+    //Add active class to current link
+    var $activeLink = $('#hero-nav a[href="' +current+'"]');
+    $activeLink.parent().addClass('uk-active');
+
+    $activeLink = $('#nav a[href="' +current+ '"]');
+    $activeLink.parent().addClass('uk-active');
+
+    //Add active class to parent
+    $activeLink.parents("li").addClass('uk-active');
 
     $offCanvas.on("hide", function(e) {
         $hamburger.toggleClass("is-active");
