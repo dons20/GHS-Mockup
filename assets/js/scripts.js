3ef79f9f9b643dd38dd8e581d9a0648e6f0f86b8
Keno Clayton <keno.clayton15@gmail.com>
2017-03-08 12:27:38 -0500

    More updates to navigation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,17 +5,21 @@ $(document).ready(function () {
     console.log(current);
 
     //Add active class to current link
-    var $activeLink = $('#hero-nav a[href="' +current+'"]');
-    $activeLink.parent().addClass('uk-active');
+    var $heroLink = $('#hero-nav a[href="' +current+'"]');
+    $heroLink.parent().addClass('uk-active');
 
-    $activeLink = $('#nav a[href="' +current+ '"]');
+    var $activeLink = $('#nav a[href="' +current+ '"]');
     $activeLink.parent().addClass('uk-active');
 
     //Add active class to parent
     $activeLink.parents("li").addClass('uk-active');
 
+    //Repeat for Mobile menu
+    $activeLink = $('#mobile-nav a[href="' +current+ '"]');
+    $activeLink.parents("li").addClass('uk-active');
+
     $offCanvas.on("hide", function(e) {
-        $hamburger.toggleClass("is-active");
+        $hamburger.removeClass("is-active");
     })
     $hamburger.on("click", function(e) {
         $hamburger.toggleClass("is-active");
