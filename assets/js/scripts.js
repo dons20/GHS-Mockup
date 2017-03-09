0c167ed44f0aad0c695a7c276d67a2b40d914e03
Keno Clayton <keno.clayton15@gmail.com>
2017-03-08 19:50:09 -0500

    Added notification

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,12 +7,23 @@ $(document).ready(function () {
     $("body").css("display", "none");
     $("body").fadeIn(250);
 
-    //Fade Out on navigation link clicks
-    $(".uk-nav a, .uk-navbar a").click(function(event) {
+    //Fade Out on navigation link clicks (not # links)
+    $(".uk-nav a[href!=\"#\"], .uk-navbar a[href!=\"#\"]").click(function(event) {
         event.preventDefault();
         linkLocation = this.href;
         $("body").fadeOut(250, redirectPage);
-    })
+    });
+
+    $(this).delay(1000).queue(function() {
+        UIkit.notification({
+            message: [  "<p class=\"uk-text-center\">The GHS Task-Force Needs You!", 
+                        "<br /><span class=\"uk-text-meta\">The GHSPSA is an informal group of past students of Glenmuir High School which meets monthly in Kingston and more frequently in the lead-up to any activity. Please feel free to join us.</span><br/></p><button class=\"uk-button uk-button-primary uk-button-small uk-width-1-1\">Join</button>"
+                        ],
+            status: 'primary',
+            pos: 'bottom-right',
+            timeout: 10000,
+        });
+    });
 
     function redirectPage() {
         window.location = linkLocation;
